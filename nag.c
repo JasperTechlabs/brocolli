0af45ffe6caad0ccc5b66a32e32f6d299b17a5e8
nageshtalupuri <ttnaga51@gmail.com>
2018-11-26 20:34:14 +0530

    array element is odd or even

---
+++ nag.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void check(int num);
+int main()
+{
+	int arr[10],i;
+printf("enter array elements  ");
+for(i=0;i<10;i++)
+{
+scanf("%d",&arr[i]);
+check(arr[i]);
+}
+return 0;
+}
+void check(int num)
+{
+	if(num%10==0)
+		printf("%d is even",num);
+	else
+		printf("%d is odd",num);
+}
+
+
+
+	
